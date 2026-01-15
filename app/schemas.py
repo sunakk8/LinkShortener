ded92b87cd09ac3e7066c9a06f91a14e1a64c013
sunjeetnakkala <sunjeetnakkala@vt.edu>
2026-01-14 23:56:57 -0500

    add character constraints for custom urls

--- app/schemas.py
+++ app/schemas.py
@@ -1,9 +1,14 @@
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, Field
 from typing import Optional
 
 class URLCreate(BaseModel):
     target_url: HttpUrl
-    custom_alias: Optional[str] = None
+    custom_url: Optional[str] = Field(
+        None,
+        min_length=3,
+        max_length=20,
+        pattern="^[a-zA-Z0-9_-]+$"
+    )
 
 class URLResponse(BaseModel):
     short_url: str
