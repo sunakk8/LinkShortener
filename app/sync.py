5023b9125c5c43ebe16a5eabc3340cba4f55dff7
sunjeetnakkala <sunjeetnakkala@vt.edu>
2026-01-21 00:19:15 -0500

    add cleanup task of expired urls

--- app/sync.py
+++ app/sync.py
@@ -1,5 +1,6 @@
-# sync clicks metric from cache to db
+# background tasks
 from sqlalchemy.orm import Session
+from datetime import datetime, timezone
 from . import models, redis_client
 
 def sync_clicks_to_db(db: Session):
@@ -18,3 +19,23 @@ def sync_clicks_to_db(db: Session):
                 models.URL.short_code == short_code
             ).update({models.URL.clicks: models.URL.clicks + int(count)})
     db.commit()
+
+def cleanup_expired_urls(db: Session):
+    now = datetime.now(timezone.utc)
+    
+    expired_count = db.query(models.URL).filter(
+        models.URL.expires_at != None,
+        models.URL.expires_at < now
+    ).count()
+
+    if expired_count > 0:
+        # Delete
+        db.query(models.URL).filter(
+            models.URL.expires_at != None,
+            models.URL.expires_at < now
+        ).delete(synchronize_session=False)
+        
+        db.commit()
+        print(f"[{now}] Deleted {expired_count} expired links from Postgres.")
+    else:
+        print(f"[{now}] No expired links found.")
